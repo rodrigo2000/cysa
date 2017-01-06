a66f67f041921f9d6773d5e416204dd8865ada4b
rodrigo.sevilla <rodrigosevilla2000@hotmail.com>
2017-01-06 13:27:44 -0600

    Interfaz de catálogos

---
+++ application/views/catalogos_view.php
@@ -0,0 +1,25 @@
+<div class="row">
+    <div class="col-sm-6">
+	<div class="card card-block">
+	    <h4 class="card-title">Procesos</h4>
+	    <p class="card-text">Listado de procesos con certificación ISO usados por la Contraloría de Mérida.</p>
+	    <a href="<?= base_url(); ?>Procesos" class="btn btn-primary">Ver catálogo</a>
+	</div>
+    </div>
+
+    <div class="col-sm-6">
+	<div class="card card-block">
+	    <h4 class="card-title">Tareas</h4>
+	    <p class="card-text">Listado de etiquetas de tareas que aparecen en la línea de tiempo.</p>
+	    <a href="<?= base_url(); ?>Tareas" class="btn btn-primary">Ver catálogo</a>
+	</div>
+    </div>
+
+    <div class="col-sm-6">
+	<div class="card card-block">
+	    <h4 class="card-title">Campos de base de datos</h4>
+	    <p class="card-text">Listado de campos de base de datos para configurar en la línea de tiempo.</p>
+	    <a href="<?= base_url(); ?>Campos" class="btn btn-primary">Ver catálogo</a>
+	</div>
+    </div>
+</div>
