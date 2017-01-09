ca77add224a80af14105148ee29e63737bdec007
rodrigo.sevilla <rodrigosevilla2000@hotmail.com>
2017-01-09 10:23:40 -0600

    Se eliminaron las funcions -- cargar_producto_no_conforme -- ajax_get_motivos_incumplimiento -- ajax_get_areas debido a que son inecesarias

--- application/controllers/Procesos.php
+++ application/controllers/Procesos.php
@@ -55,69 +55,9 @@ class Procesos extends MY_Controller {
 	parent::modificar($id, $data);
     }
 
-    function ajax_get_areas($idSubdireccion = NULL) {
-	$return = array('success' => FALSE);
-	if (empty($idSubdireccion)) {
-	    $idSubdireccion = $this->input->post("idSubdireccion");
-	}
-	$res = $this->{$this->module['controller'] . "_model"}->get_areas("sac");
-	if (count($res) > 0) {
-	    $return = array(
-		'success' => TRUE,
-		'data' => $res
-	    );
-	}
-
-	echo json_encode($return);
-    }
-
-    function ajax_get_motivos_incumplimiento($idMotivo = NULL) {
-	$return = array('success' => FALSE);
-	if (empty($idMotivo)) {
-	    $idMotivo = $this->input->post("idMotivoIncumplimiento");
-	}
-	$res = $this->{$this->module['controller'] . "_model"}->get_motivos($idMotivo);
-	if (count($res) > 0) {
-	    $return = array(
-		'success' => TRUE,
-		'data' => $res
-	    );
-	}
-
-	echo json_encode($return);
-    }
-
-    function cargar_producto_no_conforme() {
-	$dbSAC = $this->{$this->module['controller'] . "_model"}->getDatabase(APP_DATABASE_SAC);
+    function eliminar($id = 0, $data = NULL) {
 	$data = array(
-	    'modal' => TRUE,
-	    "tituloModulo" => $this->module['title_new'],
-	    "etiquetaBoton" => "Agregar",
-	    "urlAction" => $this->module['new_url'],
-	    "auditorias" => $this->{$this->module['controller'] . "_model"}->get_auditorias(0),
-	    "etapas" => $this->db->order_by("id_Proceso", "ASC")->get("cat_etapa_auditoria")->result_array(),
-	    "subdirecciones" => $dbSAC->select("DISTINCT (denSubdireccion), clv_subdir")->join("ayunta_subdireccion a", "idSubdireccion = clv_subdir", "LEFT")->where("a.clv_dir", 5)->where("activo", 1)->order_by("clv_subdir", "ASC")->get("dcont_areas_auditoria c")->result_array(),
-	    "areas" => $this->{$this->module['controller'] . "_model"}->get_areas("sac"),
-	    "motivosIncumplimiento" => $this->{$this->module['controller'] . "_model"}->get_motivos("formato"),
-	    "acciones" => $this->db->get("cat_acciones_pnc")->result_array(),
-	    "responsablesAutorizan" => $this->{$this->module['controller'] . "_model"}->get_responsables(),
-	    "documentos" => $this->{$this->module['controller'] . "_model"}->get_documentos()
-	);
-	$this->load->view("productos_nuevo_view", $data);
-    }
-
-    function eliminar($id, $data = NULL) {
-//	if ($this->isTipoCuenta(ADVANS_CUENTA_PROFESIONAL)) {
-//	    redirect(base_url() . $this->module['controller']);
-//	}
-//	if ($this->isTipoCuenta(ADVANS_CUENTA_ADMINISTRADOR) && $id == 1) {
-//	    $informacion = array('state' => 'warning', 'message' => 'El usuario <b>Administrador de Soluciones ADVANS</b> es imposible de desactivar');
-//	    $this->session->set_flashdata("informacion", $informacion);
-//	    redirect(base_url() . $this->module['controller']);
-//	}
-
-	$data = array(
-	    "etiqueta" => "¿Esta seguro que desea eliminar esta cuenta?",
+	    "etiqueta" => "¿Esta seguro que desea eliminar este proceso?",
 	    "urlActionDelete" => $this->module['delete_url'],
 	    "urlActionCancel" => $this->module['listado_url'],
 	    "id" => $id
