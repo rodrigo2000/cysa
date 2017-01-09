90d597bba1c823d1ab822f1422dadd2ffa60884e
rodrigo.sevilla <rodrigosevilla2000@hotmail.com>
2017-01-09 13:33:39 -0600

    Vista de reportes

---
+++ application/views/reportes_view.php
@@ -0,0 +1,91 @@
+<div class="row">
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Control de Auditoría</h4>
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Amplicaciones y reprogramaciones</h4>
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Fechas de Autorización</h4>
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Auditorías atendidas</h4>
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Programa de actividades</h4>
+		<p class="card-text">DETALLADO</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Programa de actividades</h4>
+		<p class="card-text"><input type="checkbox">Todos los CC</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Relación de Auditorías realizadas por período</h4>
+		<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Reporte Lectura de Actas por Período</h4>
+		<p class="card-text"><input type="checkbox">Todos los CC</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+    <div class="col-sm-4">
+	<div class="card text-center">
+	    <div class="card-block">
+		<h4 class="card-title">Reporte LGT Art. 70<br>Fracc. XXIV</h4>
+		<p class="card-text"><input type="checkbox">Todos los CC</p>
+		<a href="#" class="btn btn-primary">Descargar</a>
+	    </div>
+	</div>
+    </div>
+</div>
+<style>
+    .text-center { text-align: center; }
+    .card {height: 180px;}
+</style>
