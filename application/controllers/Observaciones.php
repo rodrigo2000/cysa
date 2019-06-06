77608065c54365b823a35e88155b6e6f40f68a27
Rodrigo Alejandro Sevilla Blanco <rodrigosevilla2000@hotmail.com>
2019-06-06 11:43:58 -0500

    CYSA: Observaciones Corrección al momento de asignar el número consecutivo de una observación nueva

--- application/controllers/Observaciones.php
+++ application/controllers/Observaciones.php
@@ -35,10 +35,13 @@ class Observaciones extends MY_Controller {
                 'observaciones_is_eliminada' => 0
             );
             if (empty($post['observaciones_id'][$index])) {
+                $numero = $this->Observaciones_model->get_siguiente_numero_de_observacion($auditorias_id);
+                $data['observaciones_numero'] = $numero;
                 $return = $this->Observaciones_model->insert($data);
                 $return['data']['accion'] = "nuevo";
                 $return['data']['message'] = "Se ha agregado la observación.";
-                $return['data']['observaciones_numero'] = $this->Observaciones_model->get_siguiente_numero_de_observacion($auditorias_id);
+                $return['data'] = array_merge($return['data'], $data);
+                $return['data']['observaciones_id'] = $return['data']['insert_id'];
             } else {
                 $return = $this->Observaciones_model->update($post['observaciones_id'][$index], $data);
                 $return['data']['accion'] = "actualizar";
@@ -48,11 +51,51 @@ class Observaciones extends MY_Controller {
                 $return['data'] = array_merge($return['data'], $data);
             }
             $return['data']['observaciones_numero'] = $post['observaciones_numero'][$index];
+            $return['data']['old_selector'] = $this->input->post("selector");
         }
         if ($return['state'] === 'success') {
             $return['success'] = TRUE;
         }
         echo json_encode($return);
+    }
+
+    function eliminar_observacion() {
+        $return = array(
+            'state' => 'error',
+            'success' => FALSE,
+            'message' => 'Error desconocido'
+        );
+        $observaciones_id = $this->input->post('observaciones_id');
+        if (!empty($observaciones_id)) {
+            if (is_numeric($observaciones_id)) {
+                $recomendaciones = $this->Observaciones_model->get_recomendaciones_de_observacion($observaciones_id);
+                if (!empty($recomendaciones)) {
+                    foreach ($recomendaciones as $r) {
+                        $this->Recomendaciones_model->delete($r['recomendaciones_id']);
+                    }
+                }
+                $return = $this->Observaciones_model->delete($observaciones_id);
+                if ($return['state'] === "success") {
+                    $return['success'] = TRUE;
+                    $return['message'] = "Se ha eliminado la observación.";
+                    $return['data'] = array(
+                        'observaciones_id' => $observaciones_id,
+                        'selector' => 'observaciones_' . $observaciones_id
+                    );
+                }
+            } else {
+                // Es una observación nueva, por lo tanto es solo eliminar la pestaña de la observación
+                $return['success'] = TRUE;
+                $return['data'] = array(
+                    'observaciones_id' => $observaciones_id,
+                    'selector' => $observaciones_id
+                );
+            }
+        } else {
+            $return['message'] = "Faltó especificar el identificador de la observación.";
+        }
+        echo json_encode($return);
+        return $return;
     }
 
 }
