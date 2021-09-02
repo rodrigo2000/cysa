881ecf5354449cb7110c92a441c2f40d3d821fe2
Rodrigo Alejandro Sevilla Blanco <rodrigosevilla2000@hotmail.com>
2021-09-02 15:13:06 -0500

    Ahora se puedes actualizar valores desde un oficio

--- application/controllers/Oficios.php
+++ application/controllers/Oficios.php
@@ -39,4 +39,39 @@ class Oficios extends MY_Controller {
         echo json_encode($return);
     }
 
+    /**
+     * Esta función permite asignar un valor directo a la base de datos
+     */
+    function asignar_valor() {
+        sleep(1);
+        $return = array(
+            'success' => FALSE,
+            'error' => NULL,
+            'valor' => NULL
+        );
+        $basedatos = $this->input->post('basedatos');
+        $tabla = $this->input->post('tabla');
+        $campo = $this->input->post('campo');
+        $where = $this->input->post('where');
+        $where_valor = $this->input->post('where_valor');
+        $valor = $this->input->post('valor');
+
+        $db = $this->getDatabase($basedatos);
+        $db->where($where, $where_valor)
+                ->set($campo, $valor)
+                ->from($tabla);
+        //echo $db->get_compiled_update();
+        //$result = $db->update();
+        if (TRUE or $db->affected_rows() > 0) {
+            // Se actualizó el registro
+            $return['success'] = TRUE;
+            $return['valor'] = strval($valor);
+        } else {
+            // no se actualizó ningún registro
+            $return['error'] = $db->error();
+        }
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($return, JSON_PRETTY_PRINT);
+    }
+
 }
